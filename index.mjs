9e98035c4ca136609674bd2344eee4c852d0d64a
NaZa123456 <nazarenoaraya2017@gmail.com>
2025-01-26 14:46:13 -0300

    intento de validacion

--- index.mjs
+++ index.mjs
@@ -6,6 +6,13 @@ import { dirname } from 'path';
 import session from 'express-session';
 
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 
 
 // Obtener la ruta del directorio actual
